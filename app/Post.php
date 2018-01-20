bcf1206b8229693c0f0d04452d5edb09a1831d04
roman08 <rmcentinela@gmail.com>
2018-01-20 16:26:25 -0600

    se agrega relación con posts -> categorias

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $dates = ['published_at'];
+
+    public function category()
+    {
+    	return $this->belongsTo(Category::class);
+    }
 }
