a3a11dbb4a60799dc13e98d765e7ee6466c372d0
roman08 <rmcentinela@gmail.com>
2018-01-26 15:20:19 -0600

    se agregan botones redes sociales

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 
-
+@section('meta-title', $post->title)
+@section('meta-description', $post->excerpt)
 @section('content')
 	  <article class="post image-w-text container">
 	    <div class="content-post">
@@ -21,10 +22,15 @@
 	        <footer class="container-flex space-between">
 	          <div class="buttons-social-media-share">
 	            <ul class="share-buttons">
-	              <li><a href="https://www.facebook.com/sharer/sharer.php?u=&t=" title="Share on Facebook" target="_blank"><img alt="Share on Facebook" src="/img/flat_web_icon_set/Facebook.png"></a></li>
-	              <li><a href="https://twitter.com/intent/tweet?source=&text=:%20" target="_blank" title="Tweet"><img alt="Tweet" src="/img/flat_web_icon_set/Twitter.png"></a></li>
-	              <li><a href="https://plus.google.com/share?url=" target="_blank" title="Share on Google+"><img alt="Share on Google+" src="/img/flat_web_icon_set/Google-plus.png"></a></li>
-	              <li><a href="http://pinterest.com/pin/create/button/?url=&description=" target="_blank" title="Pin it"><img alt="Pin it" src="/img/flat_web_icon_set/Pinterest.png"></a></li>
+	              
+	              <li><a href="https://www.facebook.com/sharer/sharer.php?u={{ request()->fullUrl() }}&t={{ $post->title }}" title="Compartit en Facebook" target="_blank"><img alt="Compartit en Facebook" src="/img/flat_web_icon_set/Facebook.png"></a></li>
+	              
+	              <li><a href="https://twitter.com/intent/tweet?url={{ request()->fullUrl() }}&text={{ $post->title }}&via=varekay88&hashtags=Zemdero" target="_blank" title="Tweet"><img alt="Tweet" src="/img/flat_web_icon_set/Twitter.png"></a></li>
+	              
+	              <li><a href="https://plus.google.com/share?url={{ request()->fullUrl() }}" target="_blank" title="Compartit en Google+"><img alt="Share on Google+" src="/img/flat_web_icon_set/Google-plus.png"></a></li>
+	              
+	              <li><a href="http://pinterest.com/pin/create/button/?url={{ request()->fullUrl() }}&description={{ $post->title }}" target="_blank" title="Pin it"><img alt="Pin it" src="/img/flat_web_icon_set/Pinterest.png"></a></li>
+	            
 	            </ul>
 	          </div>
 	          <div class="tags container-flex">
